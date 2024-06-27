1889dd75214c4980e9ec75d022bd6dde429c81e4
Vinodh <vinodh.jeevanantham@gmail.com>
2024-06-27 15:48:38 +0530

    tweaks

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,6 @@ import { schema } from './schemas';
 
 export interface Env {
   DB: D1Database;
-  SHEET_ID: string;
-  SHEET_API_KEY: string;
-  SYNC_TOKEN: string;
 }
 
 export interface YogaInitialContext {
@@ -24,19 +21,6 @@ export default {
     const datasources = {
       googleSheetDataSource: new GoogleSheetDataSource({ db }),
     };
-
-    if (url.pathname === '/sync-categories-and-services' && request.method === 'GET') {
-      // Sync Categories and Services
-      const token = request.headers.get('Authorization');
-      if (!token || token !== env.SYNC_TOKEN) {
-        return new Response('Unauthorized', { status: 401 });
-      }
-      try {
-        return await syncCategoriesAndServices({ request, datasources, SHEET_ID: env.SHEET_ID, SHEET_API_KEY: env.SHEET_API_KEY });
-      } catch (error) {
-        return new Response(`Failed to sync : ${error}`, { status: 500 });
-      }
-    }
 
     if (url.pathname === '/graphql') {
       const yoga = createYoga({
